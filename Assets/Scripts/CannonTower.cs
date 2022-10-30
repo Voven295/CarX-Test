f72b7318e7cbe6f0c897d1d95d505214078585ef
sqdrck <hardtohandle@yandex.ru>
2022-10-30 14:12:07 +0300

    Kekos

--- Assets/Scripts/CannonTower.cs
+++ Assets/Scripts/CannonTower.cs
@@ -27,9 +27,9 @@ namespace TowerDefence
         [SerializeField] private float reloadDuration = 1f;
         private float reloadT;
         private float bulletSpeed;
-		private float travelTime = 0.5f;
+        private float travelTime = 0.5f;
 
-		private Vector3 direction;
+        private Vector3 direction;
         private Vector3 currentTargetPoint;
 
         public void Init(PoolManager poolManager)
@@ -65,6 +65,7 @@ namespace TowerDefence
                     {
                         Shoot();
                         reloadT -= reloadDuration;
+            
                     }
                 }
             }
@@ -85,39 +86,24 @@ namespace TowerDefence
 
         public void Shoot()
         {
-			CalculateTrajectory();
+            CalculateTrajectory();
             var shell = poolManager.ReuseObject(shellPrefab, origin.transform.position, Quaternion.identity);
             shell.gameObject.GetComponent<CannonProjectile>().Init(launchPoint, launchVelocity);
         }
 
         private bool GetGhostPosition(float seconds, out Vector3 result)
         {
-			// TODO(sqdrck): Proper ghost calculations based on waypoints.
-            result = new Vector3(targetEnemy.transform.position.x - Enemy.Speed * seconds,
-                targetEnemy.transform.position.y,
-                targetEnemy.transform.position.z);
-
-            return Vector3.Distance(transform.position, result) <= enemyFinder.MaxDistance;
+            result = targetEnemy.GetFuturePosition(targetEnemy.Pp, seconds * Enemy.Speed).currentPosition;
+            return Vector3.Distance(origin.position, result) <= enemyFinder.MaxDistance;
         }
 
         private bool CalculateTrajectory()
         {
             if (!GetGhostPosition(travelTime, out currentTargetPoint)) return false;
 
-			var dist = Vector3.Distance(origin.position, currentTargetPoint);
-			bulletSpeed = dist / travelTime;
-			Debug.Log("Bullet speed = " + bulletSpeed);
-
-			direction = (currentTargetPoint - origin.position).normalized;
-
-			// TODO(sqdrck): Rotation.
-            //Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.y));
-            //var cannonBaseEulerAngles = cannonBase.transform.rotation.eulerAngles;
-            //Quaternion cannonBaseTargetRotation = Quaternion.Euler(cannonBaseEulerAngles.x, lookRotation.eulerAngles.y,
-                //cannonBaseEulerAngles.z);
-            //Quaternion cannonTargetRotation = Quaternion.Euler(lookRotation.eulerAngles.x, cannonBaseEulerAngles.y,
-                //cannonBaseEulerAngles.z);
-            //Rotate(cannonBaseTargetRotation, cannonTargetRotation);
+            var dist = Vector3.Distance(origin.position, currentTargetPoint);
+            bulletSpeed = dist / travelTime;
+            direction = (currentTargetPoint - origin.position).normalized;
 
             launchVelocity = direction * bulletSpeed;
 
@@ -132,7 +118,7 @@ namespace TowerDefence
             Gizmos.DrawSphere(targetEnemy.transform.position, 0.5f);
             Gizmos.color = Color.white;
             Gizmos.DrawSphere(currentTargetPoint, 0.5f);
-			Gizmos.DrawLine(origin.position, origin.position + direction * 5);
+            Gizmos.DrawLine(origin.position, origin.position + direction * 5);
         }
     }
 }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,14 +5,22 @@ namespace TowerDefence
 {
     public class Enemy : MonoBehaviour, IPooledObject
     {
-        private Vector3[] path;
+        public struct PathPosition
+        {
+            public Vector3[] path;
+            public Vector3 currentPosition;
+            public int currentSegment;
+            public float currentT;
+            public bool completed;
+        }
+
+        public PathPosition Pp => pp;
         private bool isInit = false;
         private bool isMoving;
 
-        public const float Speed = 6f;
+        public const float Speed = 10f;
 
-        private float pathSegmentT;
-        private int currentPathSegment;
+        PathPosition pp;
 
         public bool IsReadyToReuse { get; private set; } = true;
         public event Action OnDisabled;
@@ -25,39 +33,65 @@ namespace TowerDefence
         public void Init(Vector3[] path)
         {
             isInit = true;
-            this.path = path;
+            pp = new PathPosition();
+            pp.path = path;
             StartMoving();
         }
 
         private void StartMoving()
         {
-            currentPathSegment = 0;
-            pathSegmentT = 0;
-            transform.position = path[0];
+            pp.currentSegment = 0;
+            pp.currentT = 0;
+            pp.completed = false;
+            pp.currentPosition = pp.path[0];
+            transform.position = pp.path[0];
             isMoving = true;
             IsReadyToReuse = false;
+        }
+
+
+        public PathPosition GetFuturePosition(PathPosition pp, float skipT)
+        {
+            if (pp.completed) return pp;
+            var segmentsDist = Vector3.Distance(pp.path[pp.currentSegment + 1], pp.path[pp.currentSegment]);
+            pp.currentT += skipT / segmentsDist;
+
+            if (pp.currentT >= 1)
+            {
+                pp.currentSegment++;
+                pp.currentT -= 1;
+            }
+
+            if (pp.path.Length - 1 == pp.currentSegment)
+            {
+                pp.completed = true;
+                pp.currentPosition = pp.path[pp.currentSegment];
+            }
+            else
+            {
+                pp.currentPosition = Vector3.Lerp(pp.path[pp.currentSegment], pp.path[pp.currentSegment + 1], pp.currentT);
+            }
+            return pp;
         }
 
         private void Update()
         {
             if (!(isInit && isMoving)) return;
 
-            var segmentsDist = Vector3.Distance(path[currentPathSegment + 1], path[currentPathSegment]);
-
-            pathSegmentT += Time.deltaTime * Speed / segmentsDist;
-            transform.position = Vector3.Lerp(path[currentPathSegment], path[currentPathSegment + 1], pathSegmentT);
-
-            if (pathSegmentT >= 1)
+            var newPp = GetFuturePosition(pp, Time.deltaTime * Speed);
+            pp = newPp;
+            transform.position = pp.currentPosition;
+            if (pp.completed)
             {
-                pathSegmentT -= 1f;
-                currentPathSegment++;
-                if (path.Length == currentPathSegment)
-                {
-                    // NOTE(sqdrck): Reached.
-                    isMoving = false;
-                    IsReadyToReuse = true;
-                }
+                // NOTE(sqdrck): Reached.
+                isMoving = false;
+                IsReadyToReuse = true;
             }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawSphere(pp.path[pp.currentSegment], 0.2f);
         }
 
         public void ObjectReuse()
